c51f6d53effe4c28910183d9f329a8e77f78f2f6
Patrick Collins <patrickdcollinspdc@gmail.com>
2021-12-14 13:20:37 -0800

    added a test

--- test_A_star_pathfinding_helper.py
+++ test_A_star_pathfinding_helper.py
@@ -2,3 +2,5 @@ import A_star_pathfinding_helper
 
 def test_h():
     assert A_star_pathfinding_helper.h((10,10),(20,20)) == 20
+def test_get_clicked_pos():
+    assert A_star_pathfinding_helper.get_clicked_pos((1,1),50,800) == (16,16)
