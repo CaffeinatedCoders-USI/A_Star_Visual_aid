937e1a4de6f6242fa91c15917e5157506a9fe007
Patrick Collins <patrickdcollinspdc@gmail.com>
2021-12-14 13:24:56 -0800

    fix second test

--- test_A_star_pathfinding_helper.py
+++ test_A_star_pathfinding_helper.py
@@ -3,4 +3,4 @@ import A_star_pathfinding_helper
 def test_h():
     assert A_star_pathfinding_helper.h((10,10),(20,20)) == 20
 def test_get_clicked_pos():
-    assert A_star_pathfinding_helper.get_clicked_pos((1,1),50,800) == (16,16)
+    assert A_star_pathfinding_helper.get_clicked_pos((16,16),50,800) == (1,1)
